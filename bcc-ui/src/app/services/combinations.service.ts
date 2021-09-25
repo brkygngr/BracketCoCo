c0d85534fbcc6b974ed2ae45a69d067ddab40e3b
brkygngr <berkaygungor.1996@gmail.com>
2021-09-25 16:40:59 +0300

    add combinations svc to bcc ui

---
+++ bcc-ui/src/app/services/combinations.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CombinationsService {
+  private static readonly SERVICE_URL = 'api/combinations/'
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getBracketCombinations(pairCount: number): Observable<number> {
+    return this.httpClient.get<number>(
+      `${CombinationsService.SERVICE_URL}brackets?pairCount=${pairCount}`
+    )
+  }
+}
