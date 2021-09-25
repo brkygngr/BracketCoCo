6a071cf3cb431615102ea2b23eed3a7f7c3674fd
brkygngr <berkaygungor.1996@gmail.com>
2021-09-25 16:41:59 +0300

    remove slash from application path

--- src/main/java/com/brkygngr/bracketcoco/BracketCoCoApplication.java
+++ src/main/java/com/brkygngr/bracketcoco/BracketCoCoApplication.java
@@ -3,6 +3,6 @@ package com.brkygngr.bracketcoco;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("api")
 public class BracketCoCoApplication extends Application {
 }
