13880733426701a194af228b865aee31b5d0b1c0
brkygngr <berkaygungor.1996@gmail.com>
2021-09-25 18:56:28 +0300

    add bracket pair combinations validator to bracket pair combination

--- src/main/java/com/brkygngr/bracketcoco/ejb/combinations/pair/bracket/BracketPairCombinations.java
+++ src/main/java/com/brkygngr/bracketcoco/ejb/combinations/pair/bracket/BracketPairCombinations.java
@@ -1,13 +1,31 @@
 package com.brkygngr.bracketcoco.ejb.combinations.pair.bracket;
 
 import com.brkygngr.bracketcoco.ejb.combinations.pair.PairCombinations;
+import com.brkygngr.bracketcoco.ejb.combinations.pair.PairCombinationsValidator;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 @Stateless
 public class BracketPairCombinations implements PairCombinations {
+
+  private static final String ILLEGAL_PAIR_COUNT = "Count must be between 0 and " + Integer.MAX_VALUE;
+
+  private final PairCombinationsValidator pairCombinationsValidator;
+
+  @Inject
+  public BracketPairCombinations(PairCombinationsValidator pairCombinationsValidator) {
+    this.pairCombinationsValidator = pairCombinationsValidator;
+  }
+
   @Override
-  public int totalCombinations(int pairCount) {
+  public int totalCombinations(final int pairCount) {
+    boolean notValid = !pairCombinationsValidator.valid(pairCount);
+
+    if (notValid) {
+      throw new IllegalArgumentException(ILLEGAL_PAIR_COUNT);
+    }
+
     return 0;
   }
 }
