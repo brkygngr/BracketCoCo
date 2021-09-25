2c740a76ad7c95fecbd9d7293dda7d1fea86cb1c
brkygngr <berkaygungor.1996@gmail.com>
2021-09-25 12:09:08 +0300

    add api application class

---
+++ src/main/java/com/brkygngr/bracketcoco/BracketCoCoApplication.java
@@ -0,0 +1,8 @@
+package com.brkygngr.bracketcoco;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class BracketCoCoApplication extends Application {
+}
