5a3a9adab86ead3f1ff7ea0298d3be9ff14b4847
brkygngr <berkaygungor.1996@gmail.com>
2021-09-25 18:13:19 +0300

    rename class to omit bean word

--- src/main/java/com/brkygngr/bracketcoco/ejb/combinations/pair/bracket/BracketPairCombinationsValidator.java
+++ src/main/java/com/brkygngr/bracketcoco/ejb/combinations/pair/bracket/BracketPairCombinationsValidator.java
@@ -5,7 +5,7 @@ import com.brkygngr.bracketcoco.ejb.combinations.pair.PairCombinationsValidator;
 import javax.ejb.Stateless;
 
 @Stateless
-public class BracketPairCombinationsValidatorBean implements PairCombinationsValidator {
+public class BracketPairCombinationsValidator implements PairCombinationsValidator {
   @Override
   public boolean valid(int pairCount) {
     return false;
