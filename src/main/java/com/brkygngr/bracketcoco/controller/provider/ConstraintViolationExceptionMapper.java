8dddf7e123ee48a6376c5d72db525e8e5405e36b
brkygngr <berkaygungor.1996@gmail.com>
2021-09-26 19:15:20 +0300

    add constraint error mapping provider

---
+++ src/main/java/com/brkygngr/bracketcoco/controller/provider/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,26 @@
+package com.brkygngr.bracketcoco.controller.provider;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Provider
+public class ConstraintViolationExceptionMapper
+        implements ExceptionMapper<ConstraintViolationException> {
+
+  @Override
+  public Response toResponse(ConstraintViolationException exception) {
+
+    List<String> errors = exception.getConstraintViolations().stream()
+            .map(ConstraintViolation::getMessage)
+            .collect(Collectors.toList());
+
+    return Response.status(Response.Status.BAD_REQUEST).entity(errors)
+            .type(MediaType.APPLICATION_JSON).build();
+  }
+}
