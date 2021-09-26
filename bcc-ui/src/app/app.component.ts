3978ff5639a5826910e65c4c4456ed90ca6dbdde
brkygngr <berkaygungor.1996@gmail.com>
2021-09-26 20:20:47 +0300

    update ui form behaviour to reset result and show additional error

--- bcc-ui/src/app/app.component.ts
+++ bcc-ui/src/app/app.component.ts
@@ -8,23 +8,39 @@ import { CombinationsService } from './services/combinations.service';
 })
 export class AppComponent {
   pairCount = 0;
-  result = 0;
-  error = '';
+  result: number | undefined;
+  error: string | undefined;
 
   constructor(private readonly combinationsService: CombinationsService) {}
 
   onCountClick() {
     if (0 <= this.pairCount) {
-      this.result = 0;
-      this.error = '';
+      this.resetResult();
       this.combinationsService.getBracketCombinations(this.pairCount).subscribe(
         (result) => {
           this.result = result.totalCombinations;
+          this.error = undefined;
         },
-        (error) => {
-          this.error = error;
+        (failure: { e: string }) => {
+          this.result = undefined;
+          this.error = this.pairCount + ' ' + failure.e;
         }
       );
+    } else {
+      this.error = 'Number must be bigger than zero.';
     }
+  }
+
+  onNumberInputKeyUp() {
+    if (this.pairCount < 0) {
+      this.error = 'Number must be bigger than zero.';
+    } else {
+      this.error = undefined;
+    }
+  }
+
+  private resetResult() {
+    this.result = undefined;
+    this.error = undefined;
   }
 }
